36bb639edcf3d06141085798cc33aec05ab611f6
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-10-06 13:55:52 +0200

    Implement basic instructions

--- Robot Simulator/Assets/Scripts/Robot.cs
+++ Robot Simulator/Assets/Scripts/Robot.cs
@@ -1,9 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
-using Quaternion = UnityEngine.Quaternion;
-using Vector3 = UnityEngine.Vector3;
 
 public class Robot : MonoBehaviour
 {
@@ -11,77 +9,15 @@ public class Robot : MonoBehaviour
     [SerializeField] private float rotateSpeed;
     private readonly Queue<Instruction> instructions = new Queue<Instruction>();
 
-    private Instruction currentInstruction;
-    private Vector3 target;
-
-    private Vector3 startPosition;
-    private Quaternion startRotation;
 
     private void Start()
     {
-        instructions.Enqueue(new Instruction(InstructionType.Move, 10));
-        instructions.Enqueue(new Instruction(InstructionType.Rotate, 90));
-        instructions.Enqueue(new Instruction(InstructionType.Move, 10));
-        instructions.Enqueue(new Instruction(InstructionType.Rotate, -90));
-    }
-
-    private void Update()
-    {
-        CheckForInstruction();
-        if (currentInstruction == null)
-        {
-            return;
-        }
-        if (currentInstruction.InstructionType == InstructionType.Move)
-        {
-            transform.position = Vector3.Slerp(transform.position, target, speed * Time.deltaTime);
-            if (Vector3.Distance(transform.position, target) < .1f)
-            {
-                currentInstruction = null;
-            }
-        }
-        else
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(target), rotateSpeed * Time.deltaTime);
-            if (transform.rotation.eulerAngles == target)
-            {
-                currentInstruction = null;
-            }
-        }
-    }
-
-    private void CheckForInstruction()
-    {
-        if (instructions.Count > 0 && currentInstruction == null)
-        {
-            currentInstruction = instructions.Dequeue();
-            target = CalculateTarget(currentInstruction);
-            Debug.Log(target);
-            switch (currentInstruction.InstructionType)
-            {
-                case InstructionType.Rotate:
-                    startPosition = transform.position;
-                    break;
-                case InstructionType.Move:
-                    startRotation = transform.rotation;
-                    break;
-                default:
-                    throw new ArgumentException(nameof(currentInstruction.InstructionType));
-            }
-        }
-    }
-
-    private Vector3 CalculateTarget(Instruction instruction)
-    {
-        switch (instruction.InstructionType)
-        {
-            case InstructionType.Rotate:
-                return transform.rotation.eulerAngles - new Vector3(0, instruction.Value, 0);
-            case InstructionType.Move:
-                return transform.position + transform.forward * instruction.Value;
-            default:
-                throw new ArgumentException(nameof(instruction.InstructionType));
-        }
+        Instruction[] instructions = {new Instruction(InstructionType.Move, 10), 
+                                      new Instruction(InstructionType.Rotate, 90),
+                                      new Instruction(InstructionType.Move, 10),
+                                      new Instruction(InstructionType.Rotate, -90),
+                                      new Instruction(InstructionType.Move, 10)};
+        AddInstructions(instructions);
     }
 
     public void AddInstructions(Instruction[] instructions)
@@ -93,6 +29,66 @@ public class Robot : MonoBehaviour
                 this.instructions.Enqueue(instructions[i]);
             }
         }
+        CheckForInstruction();
+    }
 
+    private void CheckForInstruction()
+    {
+        if (instructions.Count <= 0) return;
+
+        Instruction currentInstruction = instructions.Dequeue();
+        Vector3 target = CalculateTarget(currentInstruction);
+        Debug.Log(target);
+        switch (currentInstruction.InstructionType)
+        {
+            case InstructionType.Rotate:
+                StartCoroutine(Rotate(target, currentInstruction.Value / rotateSpeed));
+                break;
+            case InstructionType.Move:
+                StartCoroutine(Move(target, currentInstruction.Value / speed));
+                break;
+            default:
+                throw new ArgumentException(nameof(currentInstruction.InstructionType));
+        }
+    }
+
+    private IEnumerator Rotate(Vector3 target, float time)
+    {
+        WaitForEndOfFrame wait = new WaitForEndOfFrame();
+        Quaternion fromAngle = transform.rotation;
+        Quaternion toAngle = Quaternion.Euler(target);
+        for (float t = 0f; t <= 1; t += Time.deltaTime / time)
+        {
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t);
+            yield return wait;
+        }
+
+        CheckForInstruction();
+    }
+
+    private IEnumerator Move(Vector3 target, float time)
+    {
+        WaitForEndOfFrame wait = new WaitForEndOfFrame();
+        Vector3 position = transform.position;
+        for (float t = 0f; t <= 1; t += Time.deltaTime / time)
+        {
+            transform.position = Vector3.Lerp(position, target, t);
+            yield return wait;
+        }
+        CheckForInstruction();
+    }
+
+
+    private Vector3 CalculateTarget(Instruction instruction)
+    {
+        switch (instruction.InstructionType)
+        {
+            case InstructionType.Rotate:
+                return transform.rotation.eulerAngles + new Vector3(0, instruction.Value, 0);
+            case InstructionType.Move:
+                return transform.position + transform.forward * instruction.Value;
+            default:
+                throw new ArgumentException(nameof(instruction.InstructionType));
+        }
     }
 }
