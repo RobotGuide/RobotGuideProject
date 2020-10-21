64443ca85d250332fc5e0a5b6962f1560cb4dbe2
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-10-21 13:12:18 +0200

    Implement lidar sensor

--- POC/Lidar/lib/RPLidar/src/RPLidar.cpp
+++ POC/Lidar/lib/RPLidar/src/RPLidar.cpp
@@ -38,79 +38,87 @@ RPLidar::RPLidar()
     _currentMeasurement.startBit = 0;
 }
 
-
 RPLidar::~RPLidar()
 {
     end();
 }
 
 // open the given serial interface and try to connect to the RPLIDAR
-bool RPLidar::begin(HardwareSerial &serialobj)
+bool RPLidar::begin(HardwareSerial& serialobj)
 {
-    if (isOpen()) {
-      end(); 
+    if (isOpen())
+    {
+        end();
     }
     _bined_serialdev = &serialobj;
     _bined_serialdev->end();
     _bined_serialdev->begin(RPLIDAR_SERIAL_BAUDRATE);
+    return true;
 }
 
 // close the currently opened serial interface
 void RPLidar::end()
 {
-    if (isOpen()) {
-       _bined_serialdev->end();
-       _bined_serialdev = NULL;
+    if (isOpen())
+    {
+        _bined_serialdev->end();
+        _bined_serialdev = NULL;
     }
 }
-
 
 // check whether the serial interface is opened
 bool RPLidar::isOpen()
 {
-    return _bined_serialdev?true:false; 
+    return _bined_serialdev ? true : false;
 }
 
 // ask the RPLIDAR for its health info
-u_result RPLidar::getHealth(rplidar_response_device_health_t & healthinfo, _u32 timeout)
+u_result RPLidar::getHealth(rplidar_response_device_health_t& healthinfo, _u32 timeout)
 {
     _u32 currentTs = millis();
     _u32 remainingtime;
-  
-    _u8 *infobuf = (_u8 *)&healthinfo;
+
+    _u8* infobuf = (_u8*)&healthinfo;
     _u8 recvPos = 0;
 
     rplidar_ans_header_t response_header;
-    u_result  ans;
+    u_result ans;
 
-
-    if (!isOpen()) return RESULT_OPERATION_FAIL;
+    if (!isOpen())
+        return RESULT_OPERATION_FAIL;
 
     {
-        if (IS_FAIL(ans = _sendCommand(RPLIDAR_CMD_GET_DEVICE_HEALTH, NULL, 0))) {
+        if (IS_FAIL(ans = _sendCommand(RPLIDAR_CMD_GET_DEVICE_HEALTH, NULL, 0)))
+        {
             return ans;
         }
 
-        if (IS_FAIL(ans = _waitResponseHeader(&response_header, timeout))) {
+        if (IS_FAIL(ans = _waitResponseHeader(&response_header, timeout)))
+        {
             return ans;
         }
 
         // verify whether we got a correct header
-        if (response_header.type != RPLIDAR_ANS_TYPE_DEVHEALTH) {
+        if (response_header.type != RPLIDAR_ANS_TYPE_DEVHEALTH)
+        {
             return RESULT_INVALID_DATA;
         }
 
-        if ((response_header.size) < sizeof(rplidar_response_device_health_t)) {
+        if ((response_header.size) < sizeof(rplidar_response_device_health_t))
+        {
             return RESULT_INVALID_DATA;
         }
-        
-        while ((remainingtime=millis() - currentTs) <= timeout) {
+
+        while ((remainingtime = millis() - currentTs) <= timeout)
+        {
             int currentbyte = _bined_serialdev->read();
-            if (currentbyte < 0) continue;
-            
+            if (currentbyte < 0)
+                continue;
+
             infobuf[recvPos++] = currentbyte;
 
-            if (recvPos == sizeof(rplidar_response_device_health_t)) {
+            if (recvPos == sizeof(rplidar_response_device_health_t))
+            {
                 return RESULT_OK;
             }
         }
@@ -119,54 +127,63 @@ u_result RPLidar::getHealth(rplidar_response_device_health_t & healthinfo, _u32 
 }
 
 // ask the RPLIDAR for its device info like the serial number
-u_result RPLidar::getDeviceInfo(rplidar_response_device_info_t & info, _u32 timeout )
+u_result RPLidar::getDeviceInfo(rplidar_response_device_info_t& info, _u32 timeout)
 {
-    _u8  recvPos = 0;
+    _u8 recvPos = 0;
     _u32 currentTs = millis();
     _u32 remainingtime;
-    _u8 *infobuf = (_u8*)&info;
+    _u8* infobuf = (_u8*)&info;
     rplidar_ans_header_t response_header;
-    u_result  ans;
+    u_result ans;
 
-    if (!isOpen()) return RESULT_OPERATION_FAIL;
+    if (!isOpen())
+        return RESULT_OPERATION_FAIL;
 
     {
-        if (IS_FAIL(ans = _sendCommand(RPLIDAR_CMD_GET_DEVICE_INFO,NULL,0))) {
+        if (IS_FAIL(ans = _sendCommand(RPLIDAR_CMD_GET_DEVICE_INFO, NULL, 0)))
+        {
             return ans;
         }
 
-        if (IS_FAIL(ans = _waitResponseHeader(&response_header, timeout))) {
+        if (IS_FAIL(ans = _waitResponseHeader(&response_header, timeout)))
+        {
             return ans;
         }
 
         // verify whether we got a correct header
-        if (response_header.type != RPLIDAR_ANS_TYPE_DEVINFO) {
+        if (response_header.type != RPLIDAR_ANS_TYPE_DEVINFO)
+        {
             return RESULT_INVALID_DATA;
         }
 
-        if (response_header.size < sizeof(rplidar_response_device_info_t)) {
+        if (response_header.size < sizeof(rplidar_response_device_info_t))
+        {
             return RESULT_INVALID_DATA;
         }
 
-        while ((remainingtime=millis() - currentTs) <= timeout) {
+        while ((remainingtime = millis() - currentTs) <= timeout)
+        {
             int currentbyte = _bined_serialdev->read();
-            if (currentbyte<0) continue;    
+            if (currentbyte < 0)
+                continue;
             infobuf[recvPos++] = currentbyte;
 
-            if (recvPos == sizeof(rplidar_response_device_info_t)) {
+            if (recvPos == sizeof(rplidar_response_device_info_t))
+            {
                 return RESULT_OK;
             }
         }
     }
-    
+
     return RESULT_OPERATION_TIMEOUT;
 }
 
 // stop the measurement operation
 u_result RPLidar::stop()
 {
-    if (!isOpen()) return RESULT_OPERATION_FAIL;
-    u_result ans = _sendCommand(RPLIDAR_CMD_STOP,NULL,0);
+    if (!isOpen())
+        return RESULT_OPERATION_FAIL;
+    u_result ans = _sendCommand(RPLIDAR_CMD_STOP, NULL, 0);
     return ans;
 }
 
@@ -175,26 +192,31 @@ u_result RPLidar::startScan(bool force, _u32 timeout)
 {
     u_result ans;
 
-    if (!isOpen()) return RESULT_OPERATION_FAIL;
-    
+    if (!isOpen())
+        return RESULT_OPERATION_FAIL;
+
     stop(); //force the previous operation to stop
 
     {
-        ans = _sendCommand(force?RPLIDAR_CMD_FORCE_SCAN:RPLIDAR_CMD_SCAN, NULL, 0);
-        if (IS_FAIL(ans)) return ans;
+        ans = _sendCommand(force ? RPLIDAR_CMD_FORCE_SCAN : RPLIDAR_CMD_SCAN, NULL, 0);
+        if (IS_FAIL(ans))
+            return ans;
 
         // waiting for confirmation
         rplidar_ans_header_t response_header;
-        if (IS_FAIL(ans = _waitResponseHeader(&response_header, timeout))) {
+        if (IS_FAIL(ans = _waitResponseHeader(&response_header, timeout)))
+        {
             return ans;
         }
 
         // verify whether we got a correct header
-        if (response_header.type != RPLIDAR_ANS_TYPE_MEASUREMENT) {
+        if (response_header.type != RPLIDAR_ANS_TYPE_MEASUREMENT)
+        {
             return RESULT_INVALID_DATA;
         }
 
-        if (response_header.size < sizeof(rplidar_response_measurement_node_t)) {
+        if (response_header.size < sizeof(rplidar_response_measurement_node_t))
+        {
             return RESULT_INVALID_DATA;
         }
     }
@@ -204,66 +226,73 @@ u_result RPLidar::startScan(bool force, _u32 timeout)
 // wait for one sample point to arrive
 u_result RPLidar::waitPoint(_u32 timeout)
 {
-   _u32 currentTs = millis();
-   _u32 remainingtime;
-   rplidar_response_measurement_node_t node;
-   _u8 *nodebuf = (_u8*)&node;
+    _u32 currentTs = millis();
+    _u32 remainingtime;
+    rplidar_response_measurement_node_t node;
+    _u8* nodebuf = (_u8*)&node;
 
-   _u8 recvPos = 0;
+    _u8 recvPos = 0;
 
-   while ((remainingtime=millis() - currentTs) <= timeout) {
+    while ((remainingtime = millis() - currentTs) <= timeout)
+    {
         int currentbyte = _bined_serialdev->read();
-        if (currentbyte<0) continue;
+        if (currentbyte < 0)
+            continue;
 
-        switch (recvPos) {
-            case 0: // expect the sync bit and its reverse in this byte          {
-                {
-                    _u8 tmp = (currentbyte>>1);
-                    if ( (tmp ^ currentbyte) & 0x1 ) {
-                        // pass
-                    } else {
-                        continue;
-                    }
+        switch (recvPos)
+        {
+        case 0: // expect the sync bit and its reverse in this byte          {
+        {
+            _u8 tmp = (currentbyte >> 1);
+            if ((tmp ^ currentbyte) & 0x1)
+            {
+                // pass
+            }
+            else
+            {
+                continue;
+            }
+        }
+        break;
+        case 1: // expect the highest bit to be 1
+        {
+            if (currentbyte & RPLIDAR_RESP_MEASUREMENT_CHECKBIT)
+            {
+                // pass
+            }
+            else
+            {
+                recvPos = 0;
+                continue;
+            }
+        }
+        break;
+        }
+        nodebuf[recvPos++] = currentbyte;
 
-                }
-                break;
-            case 1: // expect the highest bit to be 1
-                {
-                    if (currentbyte & RPLIDAR_RESP_MEASUREMENT_CHECKBIT) {
-                        // pass
-                    } else {
-                        recvPos = 0;
-                        continue;
-                    }
-                }
-                break;
-          }
-          nodebuf[recvPos++] = currentbyte;
+        if (recvPos == sizeof(rplidar_response_measurement_node_t))
+        {
+            // store the data ...
+            _currentMeasurement.distance = node.distance_q2 / 4.0f;
+            _currentMeasurement.angle = (node.angle_q6_checkbit >> RPLIDAR_RESP_MEASUREMENT_ANGLE_SHIFT) / 64.0f;
+            _currentMeasurement.quality = (node.sync_quality >> RPLIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
+            _currentMeasurement.startBit = (node.sync_quality & RPLIDAR_RESP_MEASUREMENT_SYNCBIT);
+            return RESULT_OK;
+        }
+    }
 
-          if (recvPos == sizeof(rplidar_response_measurement_node_t)) {
-              // store the data ...
-              _currentMeasurement.distance = node.distance_q2/4.0f;
-              _currentMeasurement.angle = (node.angle_q6_checkbit >> RPLIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f;
-              _currentMeasurement.quality = (node.sync_quality>>RPLIDAR_RESP_MEASUREMENT_QUALITY_SHIFT);
-              _currentMeasurement.startBit = (node.sync_quality & RPLIDAR_RESP_MEASUREMENT_SYNCBIT);
-              return RESULT_OK;
-          }
-        
-   }
-
-   return RESULT_OPERATION_TIMEOUT;
+    return RESULT_OPERATION_TIMEOUT;
 }
 
-
-
-u_result RPLidar::_sendCommand(_u8 cmd, const void * payload, size_t payloadsize)
+u_result RPLidar::_sendCommand(_u8 cmd, const void* payload, size_t payloadsize)
 {
 
     rplidar_cmd_packet_t pkt_header;
-    rplidar_cmd_packet_t * header = &pkt_header;
+    rplidar_cmd_packet_t* header = &pkt_header;
     _u8 checksum = 0;
 
-    if (payloadsize && payload) {
+    if (payloadsize && payload)
+    {
         cmd |= RPLIDAR_CMDFLAG_HAS_PAYLOAD;
     }
 
@@ -271,51 +300,57 @@ u_result RPLidar::_sendCommand(_u8 cmd, const void * payload, size_t payloadsize
     header->cmd_flag = cmd;
 
     // send header first
-    _bined_serialdev->write( (uint8_t *)header, 2);
+    _bined_serialdev->write((uint8_t*)header, 2);
 
-    if (cmd & RPLIDAR_CMDFLAG_HAS_PAYLOAD) {
+    if (cmd & RPLIDAR_CMDFLAG_HAS_PAYLOAD)
+    {
         checksum ^= RPLIDAR_CMD_SYNC_BYTE;
         checksum ^= cmd;
         checksum ^= (payloadsize & 0xFF);
 
         // calc checksum
-        for (size_t pos = 0; pos < payloadsize; ++pos) {
-            checksum ^= ((_u8 *)payload)[pos];
+        for (size_t pos = 0; pos < payloadsize; ++pos)
+        {
+            checksum ^= ((_u8*)payload)[pos];
         }
 
         // send size
         _u8 sizebyte = payloadsize;
-        _bined_serialdev->write((uint8_t *)&sizebyte, 1);
+        _bined_serialdev->write((uint8_t*)&sizebyte, 1);
 
         // send payload
-        _bined_serialdev->write((uint8_t *)&payload, sizebyte);
+        _bined_serialdev->write((uint8_t*)&payload, sizebyte);
 
         // send checksum
-        _bined_serialdev->write((uint8_t *)&checksum, 1);
-
+        _bined_serialdev->write((uint8_t*)&checksum, 1);
     }
 
     return RESULT_OK;
 }
 
-u_result RPLidar::_waitResponseHeader(rplidar_ans_header_t * header, _u32 timeout)
+u_result RPLidar::_waitResponseHeader(rplidar_ans_header_t* header, _u32 timeout)
 {
-    _u8  recvPos = 0;
+    _u8 recvPos = 0;
     _u32 currentTs = millis();
     _u32 remainingtime;
-    _u8 *headerbuf = (_u8*)header;
-    while ((remainingtime=millis() - currentTs) <= timeout) {
-        
+    _u8* headerbuf = (_u8*)header;
+    while ((remainingtime = millis() - currentTs) <= timeout)
+    {
+
         int currentbyte = _bined_serialdev->read();
-        if (currentbyte<0) continue;
-        switch (recvPos) {
+        if (currentbyte < 0)
+            continue;
+        switch (recvPos)
+        {
         case 0:
-            if (currentbyte != RPLIDAR_ANS_SYNC_BYTE1) {
+            if (currentbyte != RPLIDAR_ANS_SYNC_BYTE1)
+            {
                 continue;
             }
             break;
         case 1:
-            if (currentbyte != RPLIDAR_ANS_SYNC_BYTE2) {
+            if (currentbyte != RPLIDAR_ANS_SYNC_BYTE2)
+            {
                 recvPos = 0;
                 continue;
             }
@@ -323,13 +358,11 @@ u_result RPLidar::_waitResponseHeader(rplidar_ans_header_t * header, _u32 timeou
         }
         headerbuf[recvPos++] = currentbyte;
 
-        if (recvPos == sizeof(rplidar_ans_header_t)) {
+        if (recvPos == sizeof(rplidar_ans_header_t))
+        {
             return RESULT_OK;
         }
-  
-
     }
 
     return RESULT_OPERATION_TIMEOUT;
 }
-
--- POC/Lidar/src/main.cpp
+++ POC/Lidar/src/main.cpp
@@ -1,44 +1,53 @@
 #include <RPLidar.h>
 
-// You need to create an driver instance 
+// You need to create an driver instance
 RPLidar lidar;
 
-#define RPLIDAR_MOTOR 3 // The PWM pin for control the speed of RPLIDAR's motor.
-                        // This pin should connected with the RPLIDAR's MOTOCTRL signal 
-                        
-void setup() {
-  // bind the RPLIDAR driver to the arduino hardware serial
-  lidar.begin(Serial);
-  
-  // set pin modes
-  pinMode(RPLIDAR_MOTOR, OUTPUT);
+#define RPLIDAR_MOTOR 3 // The PWM pin for control the speed of RPLIDAR's motor. \
+                        // This pin should connected with the RPLIDAR's MOTOCTRL signal
+
+void setup()
+{
+    // bind the RPLIDAR driver to the arduino hardware serial
+    lidar.begin(Serial1);
+    Serial.begin(115200);
+
+    // set pin modes
+    pinMode(RPLIDAR_MOTOR, OUTPUT);
+    Serial.println("Start");
 }
 
-void loop() {
-  if (IS_OK(lidar.waitPoint())) {
-    float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
-    float angle    = lidar.getCurrentPoint().angle; //anglue value in degree
-    bool  startBit = lidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
-    byte  quality  = lidar.getCurrentPoint().quality; //quality of the current measurement
-    
-    serial.print("distance:");
-    serial.println(distance);
-    serial.print("angle: ");
-    serial.println(angle);
-    Serial.println();
-    
-  } else {
-    analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
-    
-    // try to detect RPLIDAR... 
-    rplidar_response_device_info_t info;
-    if (IS_OK(lidar.getDeviceInfo(info, 100))) {
-       // detected...
-       lidar.startScan();
-       
-       // start motor rotating at max allowed speed
-       analogWrite(RPLIDAR_MOTOR, 255);
-       delay(1000);
+void loop()
+{
+    if (IS_OK(lidar.waitPoint()))
+    {
+        float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
+        float angle = lidar.getCurrentPoint().angle;       //anglue value in degree
+        bool startBit = lidar.getCurrentPoint().startBit;  //whether this point is belong to a new scan
+        byte quality = lidar.getCurrentPoint().quality;    //quality of the current measurement
+
+        Serial.print("distance:");
+        Serial.println(distance);
+        Serial.print("angle: ");
+        Serial.println(angle);
+        Serial.println();
     }
-  }
+    else
+    {
+        Serial.println("fail");
+        analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
+
+        // try to detect RPLIDAR...
+        rplidar_response_device_info_t info;
+        if (IS_OK(lidar.getDeviceInfo(info, 100)))
+        {
+            Serial.println("got device");
+            // detected...
+            lidar.startScan();
+
+            // start motor rotating at max allowed speed
+            analogWrite(RPLIDAR_MOTOR, 255);
+            delay(1000);
+        }
+    }
 }
