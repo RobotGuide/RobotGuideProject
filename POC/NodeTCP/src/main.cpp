ed8bbaf96e306530f27ce6c6734e9a08e5ee8dcd
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-11-03 15:39:34 +0100

    Add basic protoype for tcp socket on esp8266

--- POC/NodeTCP/src/main.cpp
+++ POC/NodeTCP/src/main.cpp
@@ -1,11 +1,46 @@
-#include <Arduino.h>
-#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
+#include <SPI.h>
 
+const char* ssid = "AndroidAPa760";
+const char* password = "rzbb0008";
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+  WiFiClient client;
+  if (client.connect("192.168.43.110", 3030))
+  {
+    while (client.connected() || client.available())
+    {
+      if (client.available())
+      {
+        String line = client.readString();
+        Serial.println(line);
+        client.print("NAVS");
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+  delay(5000);
 }
