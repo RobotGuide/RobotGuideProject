5a2fa87d4cf74f1ad38adec88385f2c5fc1e4ed3
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2020-11-23 14:00:56 +0100

    Add new exceptions

--- Servercpp/ServerCPP/ServerCPP/main.cpp
+++ Servercpp/ServerCPP/ServerCPP/main.cpp
@@ -1,8 +1,5 @@
 #include "robotguide/Communication/TransportLayer/WindowsConnection.h"
 #include "robotguide/Communication/TransportLayer/WindowsListener.h"
-#include "robotguide/Communication/TransportLayer/SocketException.h"
-#include <string>
-#include <vector>
 #include <iostream>
 
 
@@ -11,8 +8,6 @@
 #undef  UNICODE
 
 using namespace robotguide::com::transportlayer;
-
-void LoopThroughAllConnections(std::vector<IConnection*>& connections, const std::string& message);
 
 int main()
 {
@@ -30,55 +25,5 @@ int main()
 
 	WindowsListener listener("127.0.0.1", "3030", hints);
 	listener.Listen(10);
-	std::cout << "Started" << std::endl;
-
-	std::string str;
-	std::vector<IConnection*> connections;
-
-	while (true)
-	{
-		try
-		{
-			const int handle = listener.Accept();
-			connections.push_back(new WindowsConnection(handle));
-			std::cout << handle << std::endl;
-		}
-		catch (SocketException& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-		LoopThroughAllConnections(connections, str);
-
-		std::getline(std::cin, str);
-		std::cout << str << std::endl;
-	}
-}
-
-void LoopThroughAllConnections(std::vector<IConnection*>& connections, const std::string& message)
-{
-	try
-	{
-		auto iterator = connections.begin();
-		while (iterator != connections.end())
-		{
-			if ((*iterator)->IsConnected())
-			{
-				(*iterator)->Send(message);
-				std::cout << "Sent: " << message << std::endl;
-				++iterator;
-			}
-			else
-			{
-				auto* const iteratorValue = *iterator;
-				iterator = connections.erase(iterator);
-				delete iteratorValue;
-			}
-		}
-		std::cout << "There are: " << connections.size() << " connections" << std::endl;
-
-	}
-	catch (SocketException& e)
-	{
-		std::cout << e.what() << std::endl;
-	}
+	return 1;
 }
