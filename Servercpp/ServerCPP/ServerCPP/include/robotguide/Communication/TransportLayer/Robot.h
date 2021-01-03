85c2e127da9d35e9285887f0e13a8d7a8d62aec7
Jeffrey van den Elshout <j.vandenelshout@student.fontys.nl>
2021-01-03 14:47:38 +0100

    Add robot classes

---
+++ Servercpp/ServerCPP/ServerCPP/include/robotguide/Communication/TransportLayer/Robot.h
@@ -0,0 +1,25 @@
+#ifndef  ROBOTGUIDE_COMMUNICATION_TRANSPORTLAYER_ROBOT_H
+#define  ROBOTGUIDE_COMMUNICATION_TRANSPORTLAYER_ROBOT_H
+
+#include "Connection.h"
+
+namespace robotguide::com::transportlayer
+{
+
+	class Robot
+	{
+	private:
+		int id;
+		Connection* connection;
+
+	public:
+		Robot(int id);
+		~Robot() = default;
+
+		int GetRobotId() const;
+		void SetConnection(Connection& connection);
+		const Connection* GetConnection() const;
+	};
+}
+
+#endif
---
+++ Servercpp/ServerCPP/ServerCPP/lib/Communication/TransportLayer/Robot.cpp
@@ -0,0 +1,22 @@
+#include "robotguide\Communication\TransportLayer\Robot.h"
+
+using namespace  robotguide::com::transportlayer;
+
+Robot::Robot(int id) : id(id), connection(nullptr)
+{
+}
+
+int Robot::GetRobotId() const
+{
+	return id;
+}
+
+void Robot::SetConnection(Connection& connection)
+{
+	this->connection = &connection;
+}
+
+const Connection* Robot::GetConnection() const
+{
+	return connection;
+}
