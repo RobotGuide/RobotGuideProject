1f63382d487e23a5e736e9c41aeffe748d0730c5
Jeffrey van den Elshout <j.vandenelshout@student.fontys.nl>
2021-01-05 14:13:56 +0100

    Add robot insturctors methods to interface

--- Servercpp/ServerCPP/ServerCPP/include/robotguide/Communication/TransportLayer/IRobotInstructor.h
+++ Servercpp/ServerCPP/ServerCPP/include/robotguide/Communication/TransportLayer/IRobotInstructor.h
@@ -1,12 +1,18 @@
 #ifndef  ROBOTGUIDE_COMMUNICATION_TRANSPORTLAYER_IROBOTINSTRUCTOR_H
 #define  ROBOTGUIDE_COMMUNICATION_TRANSPORTLAYER_IROBOTINSTRUCTOR_H
 
+#include  "Robot.h"
+
 namespace robotguide::com::transportlayer
 {
+
 	class IRobotInstructor
 	{
 	public:
 		virtual ~IRobotInstructor() = default;
+		virtual void AddRobot(Robot& robot) = 0;
+		virtual Robot* GetRobot(int id) = 0;
+		virtual int GetUniqueID() = 0;
 	};
 }
 #endif
